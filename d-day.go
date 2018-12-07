f44c043286f86997bed03daeb9ef92e473866b29
Bombwhale <solkblte@icloud.com>
2018-12-07 18:02:58 +0900

    Datetimestamp

--- d-day.go
+++ d-day.go
@@ -9,46 +9,49 @@ import (
 
 type Event struct {
 	Name string
-	Date time.Time
+
+	Date          time.Time
+	DateTimestamp string
+
 	DDay int
 }
 
 var (
 	Events []Event
+	Loc    *time.Location
 )
 
 func init() {
-	loc, _ := time.LoadLocation("Asia/Seoul")
-	Events = []Event{
-		Event{
-			Date: time.Date(2018, 12, 18, 0, 0, 0, 0, loc),
-			Name: "학생회장선거",
-		},
-		Event{
-			Date: time.Date(2018, 12, 26, 0, 0, 0, 0, loc),
-			Name: "왕운축제",
-		},
-		Event{
-			Date: time.Date(2018, 12, 31, 0, 0, 0, 0, loc),
-			Name: "방학식",
-		},
-	}
+	Loc, _ = time.LoadLocation("Asia/Seoul")
 
+	newEvent("학생회장선거", 2018, 12, 18)
+	newEvent("왕운축제", 2018, 12, 26)
+	newEvent("방학식", 2018, 12, 31)
+
+}
+
+func newEvent(name string, year, month, day int) {
+	event := Event{
+		Name: name,
+		Date: time.Date(year, time.Month(month), day, 0, 0, 0, 0, Loc),
+	}
+	event.DateTimestamp = event.Date.Format("2004-09-08")
+	Events = append(Events, event)
 }
 
 func dDay(w http.ResponseWriter) {
 	for i := range Events {
-		Events[i].DDay = int(Events[i].Date.Sub(time.Now()).Hours() / 24)
+		Events[i].DDay = int(Events[i].Date.Sub(time.Now().Local()).Hours() / 24)
 	}
 
 	format := `주요 일정
 ++++++++++
-
 {{ range . }}
-{{ .Name }} {{ .Date }}
+{{ .Name }} {{ .DateTimestamp }}
 D-{{ .DDay }}
 ---
 {{ end }}`
+
 	t := template.Must(template.New("format").Parse(format))
 
 	var tpl bytes.Buffer
