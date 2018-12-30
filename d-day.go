29476a1395823108699065d69d8c53cdfc512d9d
Bombwhale <solkblte@icloud.com>
2018-12-31 00:14:25 +0900

    Good bye, Wangunbot.

--- d-day.go
+++ d-day.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"net/http"
-	"text/template"
 	"time"
 )
 
@@ -26,11 +23,12 @@ func init() {
 	Loc, _ = time.LoadLocation("Asia/Seoul")
 
 	//newEvent("학생회장선거", 2018, 12, 18)
-	newEvent("왕운축제", 2018, 12, 26)
-	newEvent("방학식", 2018, 12, 31)
+	//newEvent("왕운축제", 2018, 12, 26)
+	//newEvent("방학식 및 왕운봇 사망", 2018, 12, 31)
 
 }
 
+/*
 func newEvent(name string, year, month, day int) {
 	event := Event{
 		Name: name,
@@ -39,25 +37,29 @@ func newEvent(name string, year, month, day int) {
 	event.DateTimestamp = fmt.Sprintf("%d-%d", month, day)
 	Events = append(Events, event)
 }
+*/
 
 func dDay(w http.ResponseWriter) {
-	now := time.Now()
-	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, Loc)
+	/*
+			now := time.Now()
+			today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, Loc)
 
-	for i := range Events {
-		Events[i].DDay = int(Events[i].Date.Sub(today).Hours() / 24)
-	}
+			for i := range Events {
+				Events[i].DDay = int(Events[i].Date.Sub(today).Hours() / 24)
+			}
 
-	format := `{{ range . }}
-{{ .DateTimestamp }} {{ .Name }} 
-D-{{ .DDay }}
-{{ end }}`
+			format := `{{ range . }}
+		{{ .DateTimestamp }} {{ .Name }}
+		D-{{ .DDay }}
+		{{ end }}`
 
-	t := template.Must(template.New("format").Parse(format))
+			t := template.Must(template.New("format").Parse(format))
 
-	var tpl bytes.Buffer
+			var tpl bytes.Buffer
 
-	t.Execute(&tpl, Events)
-	sendMessage(w, tpl.String(), home)
+			t.Execute(&tpl, Events)
+			sendMessage(w, tpl.String(), home)
+	*/
 
+	sendMessage(w, "12-31 방학식 및 왕운봇 동면\nD-Day", home)
 }
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/JedBeom/wangunbot
+
+require (
+	github.com/JedBeom/airq v0.1.0
+	github.com/JedBeom/schoolmeal v0.0.0-20181208025734-4e02ccfbcfc8
+	github.com/anaskhan96/soup v1.1.1 // indirect
+	github.com/araddon/dateparse v0.0.0-20181123171228-21df004e09ca // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jasonlvhit/gocron v0.0.0-20180312192515-54194c9749d4
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/simplereach/timeutils v1.2.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/net v0.0.0-20181220203305-927f97764cc3 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
